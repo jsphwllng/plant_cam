7e6bde8d170c05d96792c369aa4a5732d23ad7da
jsphwllng <63716060+jsphwllng@users.noreply.github.com>
2020-10-15 12:42:05 +0200

    🌻 updated comments, refactored code

--- pycam.py
+++ pycam.py
@@ -6,6 +6,7 @@ import random
 #start the webcam
 
 def capture_plant():
+    #initiates the webcam
     pygame.init()
     pygame.camera.init()
     if random.randint(0, 100) == 33:
@@ -18,8 +19,8 @@ def capture_plant():
     #sleep for one second to allow camera to focus, etc
     time.sleep(10)
     #open image and save
-
     image = cam.get_image()
     pygame.image.save(image, "image.jpg")
+    #turn off webcam
     cam.stop()
     print("image saved!")
